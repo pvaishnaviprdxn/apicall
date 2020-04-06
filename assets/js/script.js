7338e6f8da184e0b5c6e568e06d17a4f95fefe5f
vaishnavi <vaishnavip@prdxn.com>
2020-04-06 19:54:31 +0530

    function to display 1st g data done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 /* Author: 
 
 */
+
+var x = new XMLHttpRequest();
+x.open('GET','https://jsonplaceholder.typicode.com/posts',true);
+x.send();
+x.onreadystatechange = function(){ 
+    if(x.readyState == 4){
+        obj = JSON.parse(x.responseText);
+        disp(obj);
+    }
+}
+function disp(obj){
+    var mainc = document.getElementById("datad");
+    for(var i=0;i <obj.length ;i++){  
+        var maind = document.createElement("data");
+            if(i<6){
+                maind.innerHTML="<br> Title:"+obj[i].title+"<br> Body:"+obj[i].body+"<br>";
+                mainc.appendChild(maind); 
+            }  
+    }
+}
