8dff9c8c65818048cdd143aeb2b66a3d87c9cd30
vaishnavi <vaishnavip@prdxn.com>
2020-04-27 21:17:30 +0530

    functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,33 @@
-/* Author: 
-
-*/
-
+/* Author */
+var count = 6;
+var obj =[];
 var x = new XMLHttpRequest();
 x.open('GET','https://jsonplaceholder.typicode.com/posts',true);
 x.send();
 x.onreadystatechange = function(){ 
     if(x.readyState == 4){
-        obj = JSON.parse(x.responseText);
-        disp(obj);
+        obj = JSON.parse(this.responseText);
+        //console.log(obj);
+        for(i=0; i < 6; i++){
+            rows = "<tr><td>"+obj[i].id+"</td><td>"+obj[i].title+"</td><td>"+obj[i].body+"</td></tr>";
+            var tr = document.createElement("tr");
+            tr.innerHTML = rows;
+            document.getElementById("list").appendChild(tr);
+        }
+        return obj;
     }
 }
-function disp(obj){
-    var mainc = document.getElementById("datad");
-    for(var i=0;i <obj.length ;i++){  
-        var maind = document.createElement("data");
-            if(i<6){
-                maind.innerHTML="<br> Title:"+obj[i].title+"<br> Body:"+obj[i].body+"<br>";
-                mainc.appendChild(maind); 
-            }  
+document.getElementById("more").onclick =function(){
+    displayData();
+    count = count + 6; // Increment count by 6
+    console.log(count);
+    return count;
+}
+function displayData() {
+    for (i = count; i < count + 6 && i < obj.length; i++) {
+        rows = "<tr><td>"+obj[i].id+"</td><td>"+obj[i].title+"</td><td>"+obj[i].body+"</td></tr>";
+        var tr = document.createElement("tr");
+        tr.innerHTML = rows;
+        document.getElementById("list").appendChild(tr);
     }
 }
